b0abd148c7de30706d2a5a4de7f303d5816aa6ab
Friedrich von Never <friedrich@fornever.me>
2022-09-14 22:25:57 +0200

    (#3) Compiler, SDK: prepare the local development flow

---
+++ scripts/build-packages.ps1
@@ -0,0 +1,13 @@
+﻿param (
+    $SourceDirectory = "$PSScriptRoot/../",
+    $PackageLocation = "$SourceDirectory/nupkg"
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+dotnet pack $SourceDirectory/PascalABC.NET.Compiler --output $PackageLocation -p:VersionSuffix=dev
+if (!$?) { throw "dotnet pack exit code: $LASTEXITCODE" }
+
+dotnet pack $SourceDirectory/PascalABC.NET.SDK --output $PackageLocation -p:VersionSuffix=dev
+if (!$?) { throw "dotnet pack exit code: $LASTEXITCODE" }
